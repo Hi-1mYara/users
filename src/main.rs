d8735ed2e0dd1dc4b8694567a53e1dc8429502e3
Hi-1mYara <yarakoren@gmail.com>
2026-03-05 15:16:59 +0100

    minor internal restructuring
    - moved input functions for account creation into modules
    - will eventually put these modules into separate files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,60 @@
-use std::io;
+
+mod input {
+    pub mod mkacc {
+        pub use std::io;
+
+        pub fn input_username() -> String {
+            println!("Enter your username:");
+    
+            let mut username = String::new();
+    
+            io::stdin()
+                .read_line(&mut username)
+                .expect("Failed to read line");
+    
+            let username = username.trim_end().to_string();
+    
+            return username;
+        }
+
+        pub fn input_email() -> String {      // Asks for email
+            println!("Enter your email:");
+                
+            let mut email = String::new();
+
+            io::stdin()
+                .read_line(&mut email)
+                .expect("Failed to read line");
+
+            let email = email.trim_end().to_string();
+        
+            return email;
+        }
+
+        pub fn is_admin() -> bool {           // Asks for admin state
+            loop {
+                println!("Is this user an admin? (true or false)");
+            
+                let mut is_admin = String::new();
+        
+                io::stdin()
+                    .read_line(&mut is_admin)
+                    .expect("Failed to read line");
+
+                let is_admin: bool = match is_admin.trim().parse() {    // converts string to bool
+                    Ok(bool) => bool,                                   // way easier than i thought,
+                    Err(_) => {                                         // the loop is so it doesn't just 
+                        println!("Not true or false");                  // stop entirely when it's wrong
+                        continue;
+                    }
+                };
+                return is_admin;
+            }
+        }
+    }
+}
+
+pub use crate::input::mkacc::*;
 
 #[derive(Debug)]
 struct User {
@@ -27,51 +83,5 @@ fn main() {
     dbg!(&user1);
 }
 
-fn input_username() -> String {
-    println!("Enter your username:");
-    
-    let mut username = String::new();
-    
-    io::stdin()
-        .read_line(&mut username)
-        .expect("Failed to read line");
-    
-    let username = username.trim_end().to_string();
-    
-    return username;
-}
 
-fn input_email() -> String {      // Asks for email
-    println!("Enter your email:");
-    
-    let mut email = String::new();
 
-    io::stdin()
-        .read_line(&mut email)
-        .expect("Failed to read line");
-
-    let email = email.trim_end().to_string();
-    
-    return email;
-}
-
-fn is_admin() -> bool {           // Asks for admin state
-    loop {
-        println!("Is this user an admin? (true or false)");
-        
-        let mut is_admin = String::new();
-    
-        io::stdin()
-            .read_line(&mut is_admin)
-            .expect("Failed to read line");
-
-        let is_admin: bool = match is_admin.trim().parse() {    // converts string to bool
-            Ok(bool) => bool,                                   // way easier than i thought,
-            Err(_) => {                                         // the loop is so it doesn't just 
-                println!("Not true or false");                  // stop entirely when it's wrong
-                continue;
-            }
-        };
-        return is_admin;
-    }
-}
