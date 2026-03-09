58d7d72694395f5111f12edbafef2aff6cc20783
Hi-1mYara <yarakoren@gmail.com>
2026-03-09 22:47:53 +0100

    User struct documentation

--- src/types/user.rs
+++ src/types/user.rs
@@ -1,3 +1,23 @@
+/// A struct to store user data
+/// 
+/// # Examples
+/// 
+/// A function to make a user and print its contents
+/// ```no_run
+/// fn main() -> User {
+///     let user = User::new("test", 1000, "test@example.com", false);
+///     println!(
+///         " active: {}\n username: {}\n email: {}\n uuid: {}\n admin: {}\n",
+///         user.active,
+///         user.username,
+///         user.email,
+///         user.uuid,
+///         user.admin,
+///     );
+///     return user;
+/// }
+/// ```
+
 pub struct User {
     pub active: bool,
     pub username: String,
